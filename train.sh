6a9494eda89c4362de3521334c8aab74557a1929
fedemarchits <76837490+fedemarchits@users.noreply.github.com>
2026-01-20 17:51:00 +0100

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# train.sh
+
+# 1. Login to WandB (Optional: pass key as env var)
+# wandb login $WANDB_API_KEY 
+
+# 2. Run the code
+# $1 is the config file passed from run_docker.sh
+echo "Running Training inside Docker..."
+python main.py --config configs/${1}
