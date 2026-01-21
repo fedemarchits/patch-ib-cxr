338d6af2fd99c0feeed99e816f6f12488c05ba77
Federico Marchi <federicomarchi@Federicos-MacBook-Pro.local>
2026-01-21 17:01:26 +0100

    UPupdate

--- train.sh
+++ train.sh
@@ -4,8 +4,6 @@
 # $1: Config file name (e.g., model_A_baseline.yaml)
 # $2: Optional flags (e.g., --dry_run)
 
-export WANDB_API_KEY=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
-
 echo "Running Training inside Docker..."
 echo "Config: configs/${1}"
 echo "Flags: ${2}"
