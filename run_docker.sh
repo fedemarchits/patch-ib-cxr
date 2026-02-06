6dbf11cda49a605aa9f95127f44e4c5a5f3b0c3e
Federico Marchi <federicomarchi@Federicos-MacBook-Pro.local>
2026-02-06 12:16:54 +0100

    fixes

--- run_docker.sh
+++ run_docker.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # run_docker.sh
+# Usage:
+#   ./run_docker.sh train.sh <config> [args...]     # Training
+#   ./run_docker.sh eval.sh <config> <checkpoint>   # Evaluation
 
 export WANDB_API_KEY="YOUR_API_KEY_HERE"
 export WANDB_HTTP_TIMEOUT=120
@@ -8,7 +11,11 @@ PHYS_DIR=$(pwd)
 LLM_CACHE_DIR="/llms"
 DOCKER_INTERNAL_CACHE_DIR="/llms"
 
-chmod +x "$PHYS_DIR/train.sh"
+# First argument is the script to run (default: train.sh)
+SCRIPT=${1:-train.sh}
+shift
+
+chmod +x "$PHYS_DIR/$SCRIPT"
 
 docker run \
     -v "$PHYS_DIR":/workspace \
@@ -22,4 +29,4 @@ docker run \
     --shm-size="8g" \
     --gpus '"device='"$CUDA_VISIBLE_DEVICES"'"' \
     patch_ib_img \
-    "/workspace/train.sh" "$@"
+    "/workspace/$SCRIPT" "$@"
